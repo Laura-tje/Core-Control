53879d7c384fe378e1c9010382964cddedbfa938
Laura Delissen <lauradelissen@hotmail.com>
2026-09-21 11:47:52 +0200

    made opening for timerincrease

--- Core Control/Assets/Scripts/PressureGauge.cs
+++ Core Control/Assets/Scripts/PressureGauge.cs
@@ -7,6 +7,7 @@ public class PressureGauge : MonoBehaviour
     [SerializeField] private float timer; //0-50 is groen, 50-80 is oranje, 80-100 is rood
     [SerializeField] private GameObject gauge;
     [SerializeField] private float gaugeRotation;
+    public float timeIncrease = 1; //als iets onstabiel is, add 1 of 2 hieraan om de timer sneller te laten gaan, zodra gefixed, haal het er weer af.
     public GameObject[] features; //put the switches and stuff in here
     
     
@@ -19,24 +20,22 @@ public class PressureGauge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         gaugeRotation = -timer * 1.8f;
-
+        gauge.transform.rotation = Quaternion.Euler(0, 0, (gaugeRotation + 90));
+        
+        if (timeIncrease <= 0)
+        {
+            timeIncrease = 1;
+        }
+        
         if (timer >= 100)
         {
             Explode();
         }
         else
         {
-            timer += Time.deltaTime;
+            timer += timeIncrease * Time.deltaTime;
         }
-        
-        gauge.transform.rotation = Quaternion.Euler(0, 0, (gaugeRotation + 90));
-    }
-
-    private int lookForNewProblem() //returned number from features array om nieuw probleem te zoeken, trust the process
-    {
-        return UnityEngine.Random.Range(0,features.Length);
     }
 
     private void Explode()

d6576dbdebf703f0ad515c5340a7f6827916f82d
Laura Delissen <lauradelissen@hotmail.com>
2026-09-21 13:19:44 +0200

    Fixed pressure gauge rotation

--- Core Control/Assets/Scripts/PressureGauge.cs
+++ Core Control/Assets/Scripts/PressureGauge.cs
@@ -7,27 +7,38 @@ public class PressureGauge : MonoBehaviour
     [SerializeField] private float timer; //0-50 is groen, 50-80 is oranje, 80-100 is rood
     [SerializeField] private GameObject gauge;
     [SerializeField] private float gaugeRotation;
+
+    [SerializeField] private float gaugeMin;
+    [SerializeField] private float gaugeMax;
+
     public float timeIncrease = 1; //als iets onstabiel is, add 1 of 2 hieraan om de timer sneller te laten gaan, zodra gefixed, haal het er weer af.
     public GameObject[] features; //put the switches and stuff in here
-    
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private float startingRotation = 145f;
+
+
     void Start()
     {
-        timer = 0; //0-50 is groen, 50-80 is oranje, 80-100 is rood
+        timer = 0;
+
+        startingRotation = gauge.transform.localEulerAngles.z;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        gaugeRotation = -timer * 1.8f;
-        gauge.transform.rotation = Quaternion.Euler(0, 0, (gaugeRotation + 90));
-        
+        gaugeRotation = Mathf.Lerp(gaugeMin, gaugeMax, timer / 100f);
+
+        gauge.transform.localRotation = Quaternion.Euler(
+            0,
+            0,
+            startingRotation + gaugeRotation
+        );
+
         if (timeIncrease <= 0)
         {
             timeIncrease = 1;
         }
-        
+
         if (timer >= 100)
         {
             Explode();
